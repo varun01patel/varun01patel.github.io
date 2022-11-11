963f7b9602749d82fdb6cb44eee1dd0efb836f7a
varun01patel <99588105+varun01patel@users.noreply.github.com>
2022-11-11 20:33:48 +0530

    project

---
+++ calculator/logic.js
@@ -0,0 +1,24 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+       console.log('button text is',buttonText)
+       if(buttonText=='X'){
+        buttonText= '*';
+        screenValue += buttonText;
+        screen.value += buttonText;
+       }
+       else if(buttonText=='C'){
+        screenValue = '';
+        screen.value = '';
+       }else if(buttonText=='='){
+       screen.value = eval(screenValue);
+       }
+       else{
+        screenValue += buttonText;
+        screen.value += buttonText;
+       }
+})
+}
